7c0990ae73cfa78afeb1cbd494886bd7e48249b9
alchemicjon <jrichmond124@gmail.com>
2025-12-11 11:10:09 -0500

    updates, new keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>yf", function()
+  vim.fn.setreg(vim.v.register, vim.fn.expand("%") .. ":" .. vim.fn.line("."))
+end, { desc = "Copy relative filename+line to clipboard" })
